8fb46eb0bfe535c26dfc6da9e64977cbb9028c40
Rouza mousa <rouza_124@live.se>
2020-09-29 15:31:53 +0200

    Commitment

--- src/components/TextContainer/TextContainer.js
+++ src/components/TextContainer/TextContainer.js
@@ -8,7 +8,7 @@ const TextContainer = ({ users }) => (
   <div className="textContainer">
     {users ? (
       <div>
-        <h1>People currently chatting:</h1>
+        <h1>People currently Online:</h1>
         <div className="activeContainer">
           <h2>
             {users.map(({ name }) => (
